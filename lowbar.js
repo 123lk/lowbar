de8c1c669151c4720ea2797ad5a1db25abc7def0
laura <laura_kenny85@hotmail.com>
2017-05-15 10:15:58 +0100

    implement identity, first and last

--- lowbar.js
+++ lowbar.js
@@ -1,5 +1,17 @@
 const _ = {};
 
+_.identity = function (x) {
+  return x;
+};
+
+_.first = function (array) {
+  return array[0];
+};
+
+_.last = function (array) {
+  return array[array.length - 1];
+};
+
 if (typeof module !== 'undefined') {
   module.exports = _;
 }
--- spec/lowbar.spec.js
+++ spec/lowbar.spec.js
@@ -4,9 +4,43 @@ const expect = require('chai').expect;
 
 const _ = require(path.join(__dirname, '..', './lowbar.js'));
 
-describe('_', function () {
+describe('_', () => {
   'use strict';
   it('is an object', function () {
     expect(_).to.be.an('object');
   });
+
+  describe('identity', () => {
+    it('is a function', () => {
+      expect(_.identity).to.be.a('function');
+    });
+    it('returns the same value that is used as the argument', () => {
+      let actual = _.identity('foo');
+      let expected = 'foo';
+      expect(actual).to.equal(expected);
+    });
+  });
+
+  describe('first', () => {
+    it('is a function', () => {
+      expect(_.first).to.be.a('function');
+    });
+    it('returns the first element of an array', () => {
+      let actual = _.first([1,2,3]);
+      let expected = 1;
+      expect(actual).to.equal(expected);
+    });
+  });
+
+  describe('last', () => {
+    it('is a function', () => {
+      expect(_.last).to.be.a('function');
+    });
+    it('returns the last element of an array', () => {
+      let actual = _.last([1,2,3]);
+      let expected = 3;
+      expect(actual).to.equal(expected);
+    });
+  });
+
 });
